0e2d35864a3f95622ce87f7bcbb2747d760acba7
Broken moon <quyen.luminous@gmail.com>
2024-11-14 01:02:57 +0700

    (+) passkey work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,135 @@
-import Image from "next/image";
+"use client";
 
+import {
+    BundlerClient,
+    createBundlerClient,
+    createWebAuthnCredential,
+    toCoinbaseSmartAccount,
+    toWebAuthnAccount
+} from 'viem/account-abstraction'
+import {createPublicClient, http} from "viem";
+import {arbitrumSepolia} from "viem/chains";
+import {useState} from "react";
+import type {SmartAccount} from "viem/account-abstraction/accounts/types";
+const loadCredential = () => JSON.parse(localStorage.getItem('webauthnCredential'));
+const saveCredential = (credential: any) => localStorage.setItem('webauthnCredential', JSON.stringify(credential));
+
+enum WalletType {
+    DEFAULT = 'DEFAULT',
+    EOA = 'EOA',
+    PASSKEY = 'PASSKEY',
+    NONE = 'NONE'
+}
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const currentChain = arbitrumSepolia
+    const client = createPublicClient({
+        chain: currentChain,
+        transport: http(),
+        // transport: http(process.env.BUNDLER_ENDPOINT as string),
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    })
+    const bundlerClient = createBundlerClient({
+        client,
+        transport: http(process.env.NEXT_PUBLIC_BUNDLER_ENDPOINT as string),
+    })
+
+    const [smartAccount, setSmartAccount] = useState<SmartAccount>(null)
+    const [currentWalletType, setCurrentWalletType] = useState<WalletType>(WalletType.NONE);
+    // const [bundlerClient, setBundlerClient] = useState<BundlerClient>(createBundlerClient({
+    //     client,
+    //     transport: http(process.env.BUNDLER_ENDPOINT as string),
+    // }))
+    const [passkeyPublicKey, setPasskeyPublicKey] = useState<string>('')
+    const [transactionHistory, setTransactionHistory] = useState<string[]>([])
+    async function initPasskeyWallet() {
+        let credential = loadCredential();
+
+        // If no credential exists, create a new one and save it
+        if (!credential) {
+            credential = await createWebAuthnCredential({ name: 'Wallet' });
+            saveCredential(credential);
+        }
+
+        const owner = toWebAuthnAccount({ credential })
+        setPasskeyPublicKey(owner.publicKey)
+
+        const account = await toCoinbaseSmartAccount({
+            client,
+            owners: [owner],
+        })
+        console.log('account: ', account.address)
+        setSmartAccount(account)
+        setCurrentWalletType(WalletType.PASSKEY)
+        // setBundlerClient(createBundlerClient(
+        //     {
+        //         client,
+        //         transport: http(process.env.BUNDLER_ENDPOINT as string),
+        //         account,
+        //     }
+        // ));
+    }
+
+    async function createTransaction() {
+        if (!smartAccount) {
+            console.error('Smart account is not initialized')
+            return
+        }
+        const hash = await bundlerClient.sendUserOperation({
+            account: smartAccount,
+            calls: [{
+                to: '0xeaBcd21B75349c59a4177E10ed17FBf2955fE697',
+                value: 0,
+            }],
+        })
+        console.log('hash: ', hash)
+        const receipt = await bundlerClient.waitForUserOperationReceipt({
+            hash
+        })
+
+        console.log('receipt: ', receipt)
+        setTransactionHistory([...transactionHistory, receipt.receipt.transactionHash])
+    }
+
+  return (
+      <div className="container mx-auto">
+          <div className="grid grid-rows-3 grid-flow-col gap-4">
+            <div className="row-span-3 ...">
+                <h2>Current chain:</h2>
+                <h3>{currentChain.name}</h3>
+
+                <h2>Smart account:</h2>
+                {smartAccount ? (<h3>{smartAccount.address}</h3>) : (<h3>Not initialized</h3>)}
+                <button className="btn-primary" onClick={() => createTransaction()}>Create transaction</button>
+            </div>
+              <div className="col-span-2 ...">Default wallet:
+                  <br/>
+                  <button className="btn-primary">Use wallet</button>
+              </div>
+              <div className="col-span-2 ...">Existed EOA:
+                  <br/>
+                  <button className="btn-primary">Use wallet</button>
+              </div>
+              <div className="col-span-2 ...">Passkey:
+                  <br/>
+                    {passkeyPublicKey && <h3>Public key: {passkeyPublicKey}</h3>}
+                  <br/>
+                  <button className="btn-primary" onClick={() => initPasskeyWallet()}>Use wallet</button>
+              </div>
+          </div>
+          <div>
+              Transaction history:
+              <br/>
+                <table>
+                    <tr>
+                        <th>Transaction hash</th>
+                    </tr>
+                    {transactionHistory.map((hash) => (
+                        <tr key={hash}>
+                            <td><a href={'https://sepolia.arbiscan.io/tx/'+ hash} target="_blank">{hash}</a></td>
+                        </tr>
+                    ))}
+                </table>
+          </div>
+      </div>
   );
 }
